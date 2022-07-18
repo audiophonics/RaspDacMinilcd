5fa85a1da08e5f87a1dbcc3479b62f27a086bf34
Audiophonics <webmaster@audiophonics.fr>
2022-07-18 17:15:42 +0200

    Delete moode directory

--- moode/apts_web_interface/ap_modules/lcd/kiosk_autoconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ---------------------------------------------------
-# Overwrite xinitrc
-rm -rf ~/.config/chromium/Singleton*
-rm  /home/pi/.xinitrc
-cp config_files/.xinitrc /home/pi/.xinitrc
-systemctl restart localui
-systemctl restart lcd 
